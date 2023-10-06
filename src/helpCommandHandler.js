b4eda488051318cdc507ccaf1a1fdeb352fdbe5a
Shakir Ali <shakir.ali@royalcyber.com>
2023-10-06 07:24:22 +0500

    help updated

--- src/helpCommandHandler.js
+++ src/helpCommandHandler.js
@@ -19,6 +19,10 @@ class HelpCommandHandler {
     body += "4. getDeploymentProgress <deployment_code> Example: getDeploymentProgress 1234 \n\n \n\n";
     body += "5. createDeployment <build_code> <databaseUpdateMode> <environmentCode> <strategy> Example: createDeployment 1234 NONE|UPDATE d1 ROLLING_UPDATE|RECREATE|GREEN \n\n \n\n";
     body += "6. help \n\n \n\n";
+    body += "7. getBotAdmin \n\n \n\n";
+    body += "8. updateBotAdmin <@mention> Example: updateBotAdmin @user \n\n \n\n";
+    body += "9. setupApiToken <apiToken> <subscriptionCode> Example: setupApiToken 1234 1234 \n\n \n\n";
+    body += "10. giveCreateAccess <@mention> Example: giveCreateAccess @user \n\n \n\n";
     body += "Note: \n\n"
     body += "databaseUpdateMode can be NONE or UPDATE \n\n \n\n";
     body += "strategy can be ROLLING_UPDATE or RECREATE or GREEN. \n\n \n\n";
